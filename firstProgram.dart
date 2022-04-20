ce3802df5eea2059ce43792359d1d62d93e182f1
Chetan Singh <96533473+techan-af@users.noreply.github.com>
2022-04-20 16:47:31 +0530

    First file in which I am just experimenting.

---
+++ firstProgram.dart
@@ -0,0 +1,42 @@
+// void main(){
+//   print('hello dart');
+// }
+// /** This is the main function in which most of the code is written in*/
+
+// void main(){
+//   print("   /|");
+//   print("  / |");
+//   print(" /  |");
+//   print("/___|");
+// }
+// spaces are also printed, and the execution flow is line by line
+
+// void main() {
+//   String characterName = 'Oscar';
+//   String characterAge = '35';
+// //   Integer age = 70;
+
+// //   print("There was once a man named "+characterName);
+//   print("There was once a man named ${characterName}");
+//   print("He was ${characterAge} years old");
+  
+//   characterName = 'Michael';
+  
+//   print("He like his name ${characterName}");
+//   print("But he didn't like that he was ${characterAge}");
+// }
+
+// The variables can be modified in between the program too, like in between two print statements.
+// There are 3 kinds of data types: String, Numbers and Boolean
+// Numbers can be int or double, can be negative
+
+
+ void main(){
+   String myName = 'Stanley';
+   int myNumber = 35;
+   double gpa = 24.1546;
+   bool registeredVoter = true;
+   print(myName);
+ }
+
+
